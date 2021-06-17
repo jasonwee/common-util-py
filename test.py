2810be897fcac62cef28abf46da4f79e6f2ace50
Jason Wee <peichieh@gmail.com>
2021-06-17 21:47:53 +0800

    tester

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
-import mailer
+#import mailer
+#mailer.gmail_send_text()
+#mailer.gmail_send_html()
+#mailer.mail_send_text()
+#mailer.mail_send_html()
 
-mailer.gmail()
-mailer.mail()
+import cli
+(output, error) = cli.run('echo hi')
+print(output)
+print(error)
