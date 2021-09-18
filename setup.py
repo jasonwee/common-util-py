8602550f4af46895aad16b652bf4c5e999a61464
Jason Wee <peichieh@gmail.com>
2021-09-18 22:25:17 +0800

    update long description and license

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open('LICENSE') as f:
 
 setup(
     name='common_util_py',
-    version='0.0.7',
+    version='0.0.9',
     description='common python utility modules',
-    #long_description=readme,
-    long_description='common python utility modules',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Jason Wee',
     author_email='peichieh@gmail.com',
     url='https://github.com/jasonwee/common-util-py',
-    license=license,
+    license='Apache Software License',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'pymysql',
