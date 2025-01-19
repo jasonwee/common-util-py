2b98fcc0d6f4ff4b4b4cb680d781187e1e204b7f
Jason Wee <peichieh@gmail.com>
2025-01-19 13:31:25 +0800

    build(setup): 0.0.20

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='common_util_py',
-    version='0.0.19',
+    version='0.0.20',
     description='common python utility modules',
     long_description=readme,
     long_description_content_type='text/markdown',
