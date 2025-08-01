164a3bc396e962d80a23b9bedd8c969349f43320
Jason Wee <peichieh@gmail.com>
2025-08-01 22:29:37 +0800

    upgrade to py3

--- httpclient/get/sample.py
+++ httpclient/get/sample.py
@@ -8,18 +8,18 @@ def f(x):
     return x * x;
 
 def f1(name):
-    print 'hello', name
+    print('hello', name)
 
 def info(title):
-    print title
-    print 'module name:', __name__
+    print(title)
+    print('module name:', __name__)
     if hasattr(os, 'getppid'): # only available on unix
-        print 'parent process:', os.getppid()
-    print 'process id:', os.getpid()
+        print('parent process:', os.getppid())
+    print('process id:', os.getpid())
 
 def f2(name):
     info('function f2')
-    print 'hello', name
+    print('hello', name)
 
 def f3(q):
     q.put([42, None, 'hello'])
@@ -30,7 +30,7 @@ def f4(conn):
 
 def f5(l, i):
     l.acquire()
-    print 'hello world', i
+    print('hello world', i)
     l.release()
 
 def f6(n, a):
@@ -47,32 +47,32 @@ def f7(d, l):
 
 if __name__ == '__main__':
     p = Pool(5)
-    print (p.map(f, [1,2,3]))
+    print(p.map(f, [1,2,3]))
 
     p = Process(target=f1, args=('bob',))
     p.start()
     p.join()
-    print ''
+    print('')
 
     info('main line')
     p = Process(target=f2, args=('charlie',))
     p.start()
     p.join()
-    print ''
+    print('')
 
     q = Queue()
     p = Process(target=f3, args=(q,))
     p.start()
-    print q.get()
+    print(q.get())
     p.join()
-    print ''
+    print('')
 
     parent_conn, child_conn = Pipe()
     p = Process(target=f4, args=(child_conn,))
     p.start()
-    print parent_conn.recv()
+    print(parent_conn.recv())
     p.join()
-    print ''
+    print('')
 
     lock = Lock()
     for num in range(10):
@@ -87,9 +87,9 @@ if __name__ == '__main__':
     p.start()
     p.join()
 
-    print num.value
-    print arr[:]
-    print ''
+    print(num.value)
+    print(arr[:])
+    print('')
 
     manager = Manager()
 
@@ -100,32 +100,32 @@ if __name__ == '__main__':
     p.start()
     p.join()
 
-    print d
-    print l
-    print ''
+    print(d)
+    print(l)
+    print('')
 
     pool = Pool(processes=4)
     
-    print pool.map(f, range(10))
+    print(pool.map(f, range(10)))
 
     for i in pool.imap_unordered(f, range(10)):
-        print i
+        print(i)
 
     # evaluate "f(20)" asynchronously
     res = pool.apply_async(f, (20,))
-    print res.get(timeout=1)
+    print(res.get(timeout=1))
 
     res = pool.apply_async(os.getpid, ())
-    print res.get(timeout=1)
+    print(res.get(timeout=1))
 
     multiple_results = [pool.apply_async(os.getpid, ()) for i in range(4)]
-    print [res.get(timeout=1) for res in multiple_results]
+    print([res.get(timeout=1) for res in multiple_results])
 
     res = pool.apply_async(time.sleep, (10,))
     try:
-        print res.get(timeout=1)
+        print(res.get(timeout=1))
     except TimeoutError:
-        print "We lacked patience and got a multiprocessing.TimeoutError"
+        print("We lacked patience and got a multiprocessing.TimeoutError")
 
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(logging.INFO)
