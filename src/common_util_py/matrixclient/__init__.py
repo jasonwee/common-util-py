7fd2cde6f52f1ba481deaa3cb01fbc31b12ae5db
Jason Wee <peichieh@gmail.com>
2025-08-13 20:35:37 +0800

    update header

--- src/common_util_py/matrixclient/__init__.py
+++ src/common_util_py/matrixclient/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #
-#   Copyright Jason Wee
+#   Copyright WeeTech Developer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
 #
 from .matrixclient import (
     WtMatrixClient,
